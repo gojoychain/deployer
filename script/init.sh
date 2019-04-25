2f5ad94eaad94d37b81d8fae99b468681268042e
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-25 13:38:01 +0700

    Remove wrapped var names in init

--- script/init.sh
+++ script/init.sh
@@ -3,27 +3,27 @@
 # Inject env args before calling script
 
 # Make data dir if needed
-if [ ! -d "$DATA_DIR" ]; then
+if [ ! -d $DATA_DIR ]; then
     echo "Creating data dir"
-    mkdir -p "$DATA_DIR"
+    mkdir -p $DATA_DIR
 fi
 
 # Make logs dir if needed
-if [ ! -d "$LOG_DIR" ]; then
+if [ ! -d $LOG_DIR ]; then
     echo "Creating log dir"
-    mkdir -p "$LOG_DIR"
+    mkdir -p $LOG_DIR
 fi
 
 # Create genesis block
 echo "Init genesis block"
-geth --datadir "$DATA_DIR" init "$GENESIS_FILE"
+geth --datadir $DATA_DIR init $GENESIS_FILE
 
 # Imports to the account to the datadir
 if [ ! -z "$PW_FILE" ] && [ ! -z "$PRIV_KEY_FILE" ]; then
     echo "Importing account"
-    geth --datadir "$DATA_DIR" account import --password "$PW_FILE" "$PRIV_KEY_FILE"
+    geth --datadir $DATA_DIR account import --password $PW_FILE $PRIV_KEY_FILE
 fi
 
 # Copy static-nodes.json to data dir
 echo "Copying static-node.json"
-cp "$STATIC_NODE_FILE" "$DATA_DIR/geth"
+cp $STATIC_NODE_FILE "$DATA_DIR/geth"
