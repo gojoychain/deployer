5c6bfa6028dcafb15ae01fc009c20e61e50bb964
Deric Walintukan <deric.walintukan@gmail.com>
2019-07-02 17:12:51 +0700

    Update download script

--- scripts/download-bin.sh
+++ scripts/download-bin.sh
@@ -5,12 +5,12 @@ CURRENT_VERSION=1.8.23a
 UNZIP_DIR=/tmp/gojoy
 
 echo "Downloading all geth tools..."
-wget "https://github.com/gojoychain/releases/releases/download/v$CURRENT_VERSION/geth-all-linux-amd64-$CURRENT_VERSION.tar.gz"
+wget "https://github.com/gojoychain/releases/releases/download/v$CURRENT_VERSION/all-linux-amd64-$CURRENT_VERSION.tar.gz"
 mkdir $UNZIP_DIR
-tar xvzf "geth-all-linux-amd64-$CURRENT_VERSION.tar.gz" -C $UNZIP_DIR
+tar xvzf "all-linux-amd64-$CURRENT_VERSION.tar.gz" -C $UNZIP_DIR
 chmod 755 "$UNZIP_DIR/bootnode"
 chmod 755 "$UNZIP_DIR/geth"
 sudo mv "$UNZIP_DIR/bootnode" /usr/local/bin
 sudo mv "$UNZIP_DIR/geth" /usr/local/bin
-rm "geth-all-linux-amd64-$CURRENT_VERSION.tar.gz"
+rm "all-linux-amd64-$CURRENT_VERSION.tar.gz"
 echo "Finished!"
