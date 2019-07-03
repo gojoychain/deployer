4c80b97f120ac1992a0cc276e7aea4030ba9ca51
Deric Walintukan <deric.walintukan@gmail.com>
2019-07-03 11:08:04 +0700

    Rename binary files after download

--- scripts/download-bin.sh
+++ scripts/download-bin.sh
@@ -10,7 +10,7 @@ mkdir -p $UNZIP_DIR
 tar xvzf "all-linux-amd64-$CURRENT_VERSION.tar.gz" -C $UNZIP_DIR
 chmod 755 "$UNZIP_DIR/bootnode-linux-amd64"
 chmod 755 "$UNZIP_DIR/geth-linux-amd64"
-sudo mv "$UNZIP_DIR/bootnode-linux-amd64" /usr/local/bin
-sudo mv "$UNZIP_DIR/geth-linux-amd64" /usr/local/bin
+sudo mv "$UNZIP_DIR/bootnode-linux-amd64" /usr/local/bin/bootnode
+sudo mv "$UNZIP_DIR/geth-linux-amd64" /usr/local/bin/geth
 rm "all-linux-amd64-$CURRENT_VERSION.tar.gz"
 echo "Finished!"
