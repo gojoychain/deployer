a9a271c74a129a1bd97a16e82f9b33945d5ba559
Deric Walintukan <deric.walintukan@gmail.com>
2019-12-13 14:04:20 +0700

    Remove data dir

--- docker/testnet-client-ipc/init.sh
+++ docker/testnet-client-ipc/init.sh
@@ -7,27 +7,15 @@ if [ -z "$DEPLOYER_DIR" ]; then
     exit 2
 fi
 
-# Ensure DATA_DIR was set in env
-if [ -z "$DATA_DIR" ]; then
-    echo "DATA_DIR not found in env file"
-    exit 2
-fi
-
-# Setup data dir
-if [ ! -z "$DATA_DIR" ] && [ ! -d "$DATA_DIR" ]; then
-    echo "Setting up data dir..."
-    mkdir -p "$DATA_DIR"
-fi
-
 # Create genesis block
-if [ ! -d "$DATA_DIR/geth/chaindata" ]; then
+if [ ! -d /root/.ethereum/geth/chaindata ]; then
     echo "Init genesis block..."
-    geth --datadir $DATA_DIR init /root/.joy/genesis.json
+    geth init /root/.joy/genesis.json
 fi
 
 # Copy static-nodes.json to data dir
 echo "Copying static-nodes.json..."
-cp /root/.joy/static-nodes.json "$DATA_DIR/geth"
+cp /root/.joy/static-nodes.json /root/.ethereum/geth
 
 echo "Node initialization finished!"
 
